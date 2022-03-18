d40fa3001189c241c1f762b15611cd505158c131
Michael Robards <mikerobards@icloud.com>
2022-03-18 16:12:44 -0400

    Add project files.

---
+++ BattleshipLiteNewLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteNewLibrary.Models
+{
+    public class GridSpotModel
+    {
+        public string SpotLetter { get; set; }
+        public int SpotNumber { get; set; }
+        public int Status { get; set; } 
+    }
+}
