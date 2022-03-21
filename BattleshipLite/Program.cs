fbbcf7b7113db2f3a2382a1cc0d245b57320b196
Michael Robards <mikerobards@icloud.com>
2022-03-21 16:21:04 -0400

    add WelcomeMessage method

--- BattleshipLite/Program.cs
+++ BattleshipLite/Program.cs
@@ -1,4 +1,10 @@
 ﻿
+WelcomeMessage();
 
- 
+ static void WelcomeMessage()
+{
+    Console.WriteLine("Welcome to Battleship Lite!");
+    Console.WriteLine("created by Mike Robards");
+    Console.WriteLine();
+}
 
