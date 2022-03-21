5500406fc73ab41b46fb6fdad8fb30542dde5e48
Michael Robards <mikerobards@icloud.com>
2022-03-21 16:35:19 -0400

    add AskForUsersName method

--- BattleshipLite/Program.cs
+++ BattleshipLite/Program.cs
@@ -1,10 +1,34 @@
 ﻿
+using BattleshipLiteNewLibrary.Models;
+
 WelcomeMessage();
 
- static void WelcomeMessage()
+static void WelcomeMessage()
 {
     Console.WriteLine("Welcome to Battleship Lite!");
     Console.WriteLine("created by Mike Robards");
     Console.WriteLine();
+}
+
+static PlayerInfoModel CreatePlayer()
+{
+    PlayerInfoModel output = new PlayerInfoModel();
+
+    // Ask the user for their name
+    output.UsersName = AskForUsersName();
+    // Load up the shot grid
+    // Ask the user for their 5 ship placements
+    // Clear the screen and done
+
+
+}
+
+static string AskForUsersName()
+{
+    Console.Write("What is your name: ");
+    string output = Console.ReadLine();
+
+    return output;
+
 }
 
