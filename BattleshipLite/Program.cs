dcf2846c2b72faafe99c14167bf0026a008f4ffd
Michael Robards <mikerobards@icloud.com>
2022-03-24 16:26:43 -0400

    Finish methods

--- BattleshipLite/Program.cs
+++ BattleshipLite/Program.cs
@@ -14,17 +14,73 @@ do
     DisplayShotGrid(activePlayer);
 
     // Ask activePlayer for a shot
-
-
-
     // Determine if it is a valid shot
     // Determine shot results
-    // Determine if game is over
+    RecordPlayerShot(activePlayer, opponent);
+
+    // Determine if game should continue
+    bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
+
     // If over, set activePlayer as winner
     // Else, swap positions (activePlayer to opponent)
+    if (doesGameContinue == true)
+    {
+        // swap using a temp variable
+        //PlayerInfoModel tempHolder = opponent;
+        //opponent = activePlayer;
+        //activePlayer = tempHolder;
 
+        // Using tuple
+        (activePlayer, opponent) = (opponent, activePlayer);
+
+    }
+    else
+    {
+        winner = activePlayer;
+    }
 
 } while (winner == null);
+
+IdentifyWinner(winner);
+
+static void IdentifyWinner(PlayerInfoModel winner)
+{
+    Console.WriteLine($"Congratulations to {winner.UsersName} for winning!");
+    Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots.");
+}
+
+static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+{
+
+    bool isValidShot = false;
+    string row = "";
+    int column = 0;
+
+    do
+    {
+        string shot = AskForShot();
+        (row, column) = GameLogic.SplitShotIntoRowAndColumn(shot);
+        isValidShot = GameLogic.ValidateShot(activePlayer, row, column);
+
+        if (isValidShot == false)
+        {
+            Console.WriteLine("Invalid shot location. Please try again.");
+        }
+
+    } while (isValidShot == false);
+
+    bool isAHit = GameLogic.IdentifyShotResult(opponent, row, column);
+
+    GameLogic.MarkShotResult(activePlayer, row, column, isAHit) ;
+}
+
+static string AskForShot()
+{
+    Console.Write("Please enter your shot selection: ");
+    string output = Console.ReadLine();
+    return output;
+}
 
 static void DisplayShotGrid(PlayerInfoModel activePlayer)
 {
--- BattleshipLiteNewLibrary/GameLogic.cs
+++ BattleshipLiteNewLibrary/GameLogic.cs
@@ -34,6 +34,11 @@ namespace BattleshipLiteNewLibrary
             }
         }
 
+        public static bool PlayerStillActive(PlayerInfoModel opponent)
+        {
+            throw new NotImplementedException();
+        }
+
         private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
         {
             GridSpotModel spot = new GridSpotModel
@@ -46,7 +51,32 @@ namespace BattleshipLiteNewLibrary
             model.ShotGrid.Add(spot);
         }
 
+        public static int GetShotCount(PlayerInfoModel winner)
+        {
+            throw new NotImplementedException();
+        }
+
         public static bool PlaceShip(PlayerInfoModel model, string location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool ValidateShot(PlayerInfoModel activePlayer, string row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static (string row, int column) SplitShotIntoRowAndColumn(string shot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IdentifyShotResult(PlayerInfoModel opponent, string row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void MarkShotResult(PlayerInfoModel activePlayer, string row, int column, bool isAHit)
         {
             throw new NotImplementedException();
         }
